cb4e4f4d4de5153cf062fe59ccd9c8131631f4e1
masaki <masaki@masaki-blog.net>
2020-02-24 20:00:11 +0900

    new

--- apache/priority/stop.sh
+++ apache/priority/stop.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 . ./func
 
-stop my-apache2-priority-base-sample my-running-priority-base-sample
+# stop my-apache2-priority-base-sample my-running-priority-base-sample
+stop my-apache2-priority-base-a my-running-priority-base-a
+stop my-apache2-priority-base-b my-running-priority-base-b
+stop my-apache2-priority-base-c my-running-priority-base-c
+stop my-apache2-priority-base-d my-running-priority-base-d
+# stop my-apache2-priority-base-e my-running-priority-base-e
