2ec8008f08982ec5e6a42b19899ea2ba21249bff
masaki <masaki@masaki-blog.net>
2020-02-24 18:40:50 +0900

    modified

--- apache/priority/start.sh
+++ apache/priority/start.sh
@@ -8,21 +8,7 @@ echo ======================
 docker build -t $IMAGE $DOCKER_FILE
 
 echo ======================
-echo ls
-echo ======================
-docker image ls $IMAGE
-
-echo ======================
 echo run
 echo ======================
 docker run -dit --name $CONTAINER -p $PORT:80 $IMAGE
 
-echo ======================
-echo ps
-echo ======================
-docker ps 
-
-echo ======================
-echo exec
-echo ======================
-docker exec -it $CONTAINER /bin/bash
