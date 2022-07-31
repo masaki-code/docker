642befbe3fa58826376cfa3e578b6700f46fbf78
masaki <masaki@masaki-blog.net>
2022-07-31 23:47:03 +0900

    js code

--- apache/symbol/public/index.js
+++ apache/symbol/public/index.js
@@ -3,9 +3,11 @@
         const testBtn = document.querySelector("#testBtn");
         const testBtnClick = () => {
             let href = "/php/";
-            href = href + "?test";
-            href = href + "&hoge=hoge";
-            href = href + "&piyo=" + encodeURIComponent("&");
+            href = href + "?a";
+            href = href + "&b=hoge";
+            href = href + "&c=" + encodeURIComponent("&");
+            href = href + "&d=" + encodeURIComponent("<");
+            href = href + "&e=" + encodeURIComponent(">");
 
             location.href = href;
         };
